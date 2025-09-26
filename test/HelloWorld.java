dd2be948c602b7f9786437feb3ebc507db44d0fb
pupungeggang2 <kyongkyong2892@gmail.com>
2025-09-27 00:11:56 +0900

    Start

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+		System.setProperty("org.lwjgl.librarypath", ".");
 
 		init();
 		loop();
